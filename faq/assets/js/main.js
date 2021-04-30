fed2e72f68e639618206cc9d62c46069241e5670
Pablo <pablomatematico@gmail.com>
2021-04-30 19:31:31 -0400

    act

--- faq/assets/js/main.js
+++ faq/assets/js/main.js
@@ -1,3 +1,12 @@
+function copiarAlPortapapeles(id_elemento) {
+  var aux = document.createElement("input");
+  aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+}
+
 (function(){
 	// FAQ Template - by CodyHouse.co
   var FaqTemplate = function(element) {
