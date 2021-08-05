06cf9bc6f70920a8699ace2458af9e2db716be57
Pablo Figueroa Vargas <42844957+pablodavid@users.noreply.github.com>
2021-08-05 08:24:06 -0400

    Update base.js

--- Frigorifico/base.js
+++ Frigorifico/base.js
@@ -107,7 +107,7 @@ const Funcionarios = [
 	{nombre: 'Manuel+Benavente', rut: '7993540-9', empresa: 'Frigorifico+Temuco', area: 'Seguridad'},
 	{nombre: 'Jaime+Recart', rut: '16204339-9', empresa: 'Frigorifico+Temuco', area: 'Comercial'},
 	{nombre: 'Javier+Parada', rut: '10879382-1', empresa: 'Otro', area: 'Transporte'},
-	{nombre: '', rut: '', empresa: 'Larrain', area: ''},
+	{nombre: 'Edgardo Fuentealba', rut: '8872765-7', empresa: 'Otro', area: 'Transporte'},
 	{nombre: '', rut: '', empresa: 'Larrain', area: ''},
 	{nombre: '', rut: '', empresa: 'Larrain', area: ''},
 	{nombre: '', rut: '', empresa: 'Larrain', area: ''},
