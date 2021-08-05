40f077a1d6f2eec7a0aaadead01a006056f7749f
Pablo Figueroa Vargas <42844957+pablodavid@users.noreply.github.com>
2021-08-04 23:16:50 -0400

    Add files via upload

--- Frigorifico/main.js
+++ Frigorifico/main.js
@@ -8,5 +8,11 @@ function goToForm(rut,nombre,empresa,area){
 	if(mes<10)
 		mes='0'+mes;
 	var hoy=ano+"-"+mes+"-"+dia;
-	window.open("https://docs.google.com/forms/d/e/1FAIpQLSetjxEVk83c48NaEfPofCpg9cmyugli8FlrQrjzCiI0wUcwKQ/viewform?entry.663054490="+rut+"&entry.842812461="+hoy+"&entry.234679363="+nombre+"&entry.476383136="+empresa+"&entry.476407092="+area+"&entry.673783433=No&entry.1728399202=Ninguno&entry.497685889=Vehiculo+particular", '_blank')
+	var url="https://docs.google.com/forms/d/e/1FAIpQLSetjxEVk83c48NaEfPofCpg9cmyugli8FlrQrjzCiI0wUcwKQ/viewform?entry.663054490="+rut+"&entry.842812461="+hoy+"&entry.234679363="+nombre+"&entry.476383136="+empresa
+	if(area === "Casino"||area ==="Tripería"||area ==="Jardinería"||area ==="Transporte"||area ==="Grasas"||area === "Operaciones"){
+		url=url+"&entry.476407092.other_option_response="+area
+		area="__other_option__"
+	}
+	url += "&entry.476407092="+area+"&entry.673783433=No&entry.1728399202=Ninguno&entry.497685889=Vehiculo+particular"
+	window.open(url, '_blank')
 }
